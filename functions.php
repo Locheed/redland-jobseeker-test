865626bd4a33d9c1cdc3b5874d1e84f5c70bc56e
Mika Vesasto <mika.vesasto@saunalahti.fi>
2018-10-31 19:27:50 +0200

    Moved custom post function to function.php from mu-plugins folder

--- functions.php
+++ functions.php
@@ -11,4 +11,19 @@
   add_action('after_setup_theme', 'redlab_features');
 
 
+  /* This custom post type function would be better to keep in plugins or mu-plugins folder
+  if someone would change the theme. For this Redlab test it is easier to keep in the theme folder. */
+  function redlab_post_types() {
+    register_post_type('address', array(
+      'public' => true,
+      'labels' => array(
+        'name' => 'Address',
+        'add_new_item' => 'Add New Address',
+        'edit_item' => 'Edit Address',
+        'all_items' => 'All Addresses'
+      ),
+      'menu_icon' => 'dashicons-location'
+    ));
+  }
+  add_action('init', 'redlab_post_types');
 ?>
